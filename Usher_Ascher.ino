f2cf2d757346d291b268be97bba5864b4015d4b3
Bastian Krammer <bastiankrammer@googlemail.com>
2019-07-19 12:53:26 +0200

    wip

--- Usher_Ascher.ino
+++ Usher_Ascher.ino
@@ -1,46 +1,21 @@
-/*
-
- Arduino TFT Bitmap Logo example
-
- This example reads an image file from a micro-SD card
- and draws it on the screen, at random locations.
-
- In this sketch, the Arduino logo is read from a micro-SD card.
- There is a .bmp file included with this sketch.
- - open the sketch folder (Ctrl-K or Cmd-K)
- - copy the "arduino.bmp" file to a micro-SD
- - put the SD into the SD slot of the Arduino TFT module.
-
- This example code is in the public domain.
-
- Created 19 April 2013 by Enrico Gueli
-
- http://www.arduino.cc/en/Tutorial/TFTBitmapLogo
-
- */
-
-// include the necessary libraries
+// includes 
 #include <SPI.h>
 #include <SD.h>
 #include <TFT.h>  // Arduino LCD library
 #include <String.h>
 
-// pin definition for the Uno
+// defines
 #define sd_cs  7
 #define lcd_cs 10
 #define dc     9
 #define rst    8
 
 #define debug 1
+#define FILE_PATH_LENGTH 12
+#define EXTENSION_LENGTH  4
 
-// pin definition for the Leonardo
-//#define sd_cs  8
-//#define lcd_cs 7
-//#define dc     0
-//#define rst    1
-
+// global vars
 TFT TFTscreen = TFT(lcd_cs, dc, rst);
-
 // this variable represents the image to be drawn on screen
 PImage usher_pic;
 
@@ -48,6 +23,7 @@ const char file_name[13] = "Usher/usher_";
 const char extension[5] = ".bmp";
 
 
+// arduino setup
 void setup() {
   // initialize the GLCD and show a message
   // asking the user to open the serial line
@@ -65,43 +41,51 @@ void setup() {
     TFTscreen.println(F("SD initialization failed"));
     return;
   }
-
-  // initialize and clear the GLCD screen
-  TFTscreen.begin();
-  TFTscreen.background(255, 255, 255);
 }
 
+// arduino main loop
 void loop() {
 
   for(int i=1; i<17; i++) {
-    TFTscreen.begin();
     TFTscreen.background(255, 255, 255);
 
+    // string for the incremented number (i)
     char *num;
+    // string for the complete file path
+    char *full_path;
 
-    if(i<10) {
-      num = (char *) malloc(2);
-      num[0] = i+'0';
-      num[1] = '\0';
-    }else {
-      num = (char *) malloc(4);
-      num[0] = (i/10) + '0';
-      int n = i;
-      while(n>=10) {
-        n -= 10;
-      }
-      num[1] = n + '0';
-      num[2] = '\0';
+    if( i < 10 ) {
+        full_path = (char *) malloc(FILE_PATH_LENGTH + EXTENSION_LENGTH + 1);
+        num = (char *) malloc(2);
+        num[0] = i+'0';
+        num[1] = '\0';
+    }else if( i < 100 ) {
+        full_path = (char *) malloc(FILE_PATH_LENGTH + EXTENSION_LENGTH + 2);
+        num = (char *) malloc(4);
+        num[0] = (i/10) + '0';
+        int n = i;
+        while(n>=10) {
+          n -= 10;
+        }
+        num[1] = n + '0';
+        num[2] = '\0';
+    }else if( i < 1000 ) {
+        // TODO
+//        full_path = (char *) malloc(FILE_PATH_LENGTH + EXTENSION_LENGTH + 3);
+//        num = (char *) malloc(4);
+//        num[0] = (i/10) + '0';
+//        int n = i;
+//        while(n>=10) {
+//          n -= 10;
+//        }
+//        num[1] = n + '0';
+//        num[2] = '\0';
     }
-
-    Serial.println(num);
     
-    char full_path[100];
+    // create the full path for the file
     strcpy(full_path, file_name);
     strcat(full_path, num);
     strcat(full_path, extension);
-
-    free(num);
 
 #if debug
     Serial.print("|");
@@ -116,10 +100,11 @@ void loop() {
       TFTscreen.print(F("failed to open pic"));
       TFTscreen.println(num);
       
-#if debug
+    #if debug
       Serial.print("failed to open pic:");
       Serial.println(full_path);
-#endif
+    #endif
+    
       return;
     }
     
@@ -128,7 +113,10 @@ void loop() {
 #endif
   
     TFTscreen.image(usher_pic, 0, 0);
+
+    // free the allocated space
+    free(num);
+    free(full_path);
     //delay(5000);
   }
-
 }
